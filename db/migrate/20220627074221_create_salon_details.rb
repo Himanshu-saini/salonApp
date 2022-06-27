ca869e6c1093dffcea4f3bd8ba1e0e6b17aa99e1
Himanshu <himanshusaini06@gmail.com>
2022-06-27 13:32:54 +0530

    created salon detials table

--- db/migrate/20220627074221_create_salon_details.rb
+++ db/migrate/20220627074221_create_salon_details.rb
@@ -1,7 +1,10 @@
 class CreateSalonDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :salon_details do |t|
-
+      t.string :company_name
+      t.string :PAN_no
+      t.string :address
+      t.string :total_chairs
       t.timestamps
     end
   end
