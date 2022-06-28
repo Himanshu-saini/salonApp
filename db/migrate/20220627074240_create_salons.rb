ad385d833e6c169f1a6b9defae1f33d9e844f461
Himanshu <himanshusaini06@gmail.com>
2022-06-28 15:32:32 +0530

    changes in salon table

--- db/migrate/20220627074240_create_salons.rb
+++ db/migrate/20220627074240_create_salons.rb
@@ -1,10 +1,10 @@
 class CreateSalons < ActiveRecord::Migration[6.1]
   def change
-    create_table :salons do |t|
-      t.string :company_name
-      t.string :PAN_no
+    create_table :salons, id: :bigint do |t|
+      t.string :company_name, :null => false
+      t.string :PAN_no, :null => true, index: {unique: true}
       t.string :address
-      t.string :total_chairs
+      t.integer :total_chairs, :default => 0
       t.timestamps
     end
   end
