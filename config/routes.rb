d1e3de7762ba4ffa92d3890239e9f17ed34da89b
Himanshu <himanshusaini06@gmail.com>
2022-06-28 15:37:29 +0530

    created API for salon, services and booking

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
       get 'salon' => 'salon#get_salon_detials', :defaults => {:format => 'json'}
       post 'salon' => 'salon#add_salon', :defaults => {:format => 'json'}
 
+      get 'salon/services' => 'service#get_salon_services', :defaults => {:format => 'json'}
+      post 'salon/service' => 'service#add_salon_service', :defaults => {:format => 'json'}
+
+      post 'salon/service/book' => 'service#book_service', :defaults => {:format => 'json'}
+      post 'salon/service/cancel' => 'service#cancel_service', :defaults => {:format => 'json'}
+      get 'salon/service/report' => 'service#get_services_report', :defaults => {:format => 'json'}
+
     end
   end
 end
